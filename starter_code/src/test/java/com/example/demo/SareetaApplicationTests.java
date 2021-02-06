fb26ea408daff63e8cc592c4088eea1fc9bf7589
ELGEOR-PC\elgeor <elgeor.sk@gmail.com>
2021-02-06 14:56:39 +0100

    Updates

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class SareetaApplicationTests {
 
 	@Test
